e732ef9d43f976a7e89ac3692d8779a9b69988c6
HuynhNghiax <146554474+HuynhNghiax@users.noreply.github.com>
2025-06-04 00:39:51 +0700

    d

--- src/main/java/com/example/doanltweb/dao/db/DBProperties.java
+++ src/main/java/com/example/doanltweb/dao/db/DBProperties.java
@@ -1,46 +1,31 @@
 package com.example.doanltweb.dao.db;
 
-import java.io.IOException;
-import java.util.Properties;
-
 public class DBProperties {
-    private static Properties prop = new Properties();
-
-    static {
-        try {
-            // Kiểm tra và tải file db.properties
-            if (DBProperties.class.getClassLoader().getResourceAsStream("db.properties") == null) {
-                throw new IOException("File db.properties không được tìm thấy trong classpath");
-            }
-            prop.load(DBProperties.class.getClassLoader().getResourceAsStream("db.properties"));
-        } catch (IOException e) {
-            System.err.println("Không thể tải file db.properties. Kiểm tra lại!");
-            e.printStackTrace();
-            System.exit(1); // Tắt chương trình nếu không đọc được file
-        }
-    }
-
     public static String host() {
-        return prop.get("db.host").toString();
+        return System.getenv("DB_HOST");
     }
 
     public static int port() {
         try {
-            return Integer.parseInt(prop.get("db.port").toString());
+            return Integer.parseInt(System.getenv("DB_PORT"));
         } catch (NumberFormatException e) {
-            return 3306;
+            return 3306; // Mặc định MySQL port
         }
     }
+
     public static String username() {
-        return prop.get("db.username").toString();
+        return System.getenv("DB_USERNAME");
     }
+
     public static String password() {
-        return prop.get("db.password").toString();
+        return System.getenv("DB_PASSWORD");
     }
+
     public static String dbname() {
-        return prop.get("db.dbname").toString();
+        return System.getenv("DB_NAME");
     }
+
     public static String option() {
-        return prop.get("db.option").toString();
+        return System.getenv("DB_OPTION");
     }
 }
